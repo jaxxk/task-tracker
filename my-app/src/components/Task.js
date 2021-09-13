03547941d9ef69d25f7dc74c16200ead38e6d1ff
jaxxk <forwarwick@live.com>
2021-09-12 21:13:34 -0700

    latest updates

--- my-app/src/components/Task.js
+++ my-app/src/components/Task.js
@@ -4,12 +4,12 @@ import {FaTimes} from 'react-icons/fa'
 
 const Task = ({task,onDelete,onToggle}) => {    
     return (
-        <div className='task' onDoubleClick={() => onToggle(task.id)}>
+        <div className= {`task ${task.reminder ? "reminder" : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>
                 {task.text}
                 <FaTimes style={{color:
                 'red', cursor:'pointer'}} onClick={ () => onDelete(task.id)}/> 
-                {/* //state gets passed down and actions gets passed up */}
+                {/* //state gets passed down and "actions gets passed up */}
             </h3>
             <p>{task.date}</p>
         </div>
