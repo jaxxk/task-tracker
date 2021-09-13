59c7eee7a9fbda1194da4fe9bc99fe8f6f567857
jaxxk <jackwei2018@outlook.com>
2021-09-12 21:22:58 -0700

    pulling

--- my-app/src/components/Tasks.js
+++ my-app/src/components/Tasks.js
@@ -5,6 +5,7 @@ import Task from './Task'
 const Tasks = ({tasks,onDelete,onToggle}) => {    
     return (
         //cannot do tasks.push because it is not immutable. make a new state and send it
+        //wack
         <>
           {tasks.map((task) => (
             <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
